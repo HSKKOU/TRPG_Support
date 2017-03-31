7bbd6225c48c24f4fc3db5b261f7bbf461e9c2aa
HSKKOU <hskk1120551@gmail.com>
2017-03-31 18:52:48 +0900

    Initialize Project

---
+++ Assets/Scripts/Managers/GameMainManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Managers.GameMain
+{
+  
+  public class GameMainManager : MonoBehaviour
+	{
+
+    void Awake()
+    {
+      Initialize();
+    }
+
+    /// <summary>
+    /// Initialize GameMainManager instance.
+    /// </summary>
+    private void Initialize()
+    {
+      
+    }
+	}
+
+}
---
+++ Assets/Scripts/Utils/StateMachine/StateMachine.cs
@@ -0,0 +1,38 @@
+﻿namespace Utils.StateMachine
+{
+
+	public class StateMachine<T>
+	{
+    /// <summary>
+    /// 現在のステート
+    /// </summary>
+		private State<T> currentState;
+
+		public StateMachine() { currentState = null; }
+
+		public State<T> CurrentState
+		{
+			get { return currentState; }
+		}
+
+    /// <summary>
+    /// ステートを変える
+    /// </summary>
+    /// <param name="state">State.</param>
+		public void ChangeState(State<T> state)
+		{
+			if (currentState != null) { currentState.Exit(); }
+			currentState = state;
+			currentState.Enter();
+		}
+
+    /// <summary>
+    /// 各ステートの更新はここから発火する
+    /// </summary>
+		public void Update()
+		{
+			if (currentState != null) { currentState.Execute(); }
+		}
+	}
+
+}
